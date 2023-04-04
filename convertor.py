fcbdd2fd935e1f5486351eebc794bd2c8675c8a7
Alexandr <sevrjugin@bk.ru>
2023-04-04 14:11:29 +0300

    fix

--- convertor.py
+++ convertor.py
@@ -1,43 +1,60 @@
-import telebot
-from convertor import ConvertException, Convertor
-import traceback
-from token import TOKEN
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start','help'])
-def begin(message: telebot.types.Message):
-        greet = f"Добро пожаловать, {message.chat.username}\n"
-        text = greet + 'Для начала работы введите команду в формате:'\
-                       '\n исходная валюта   валюта конвертации  количество'\
-                       '\n Например: USD eur 10 '\
-                       '\n для просмотра списка валют введите команду /valutes'
-        bot.send_message(message.chat.id,text)
-
-@bot.message_handler(commands=['valutes'])
-def begin(message: telebot.types.Message):
-        bot.reply_to(message, Convertor.get_all_valutes())
+import requests
+import xml.etree.ElementTree as ET
 
 
-@bot.message_handler(content_types=['text'])
-def converter(message: telebot.types.Message):
-    values = message.text.split(' ')
-    try:
-        if len(values) != 3:
-            raise ConvertException('Неверное количество параметров!')
-        cur1, cur2, amount = values
-        answer = Convertor.get_price(cur1.upper(), cur2.upper(), amount)
-    except ConvertException as e:
-        bot.reply_to(message, f"Ошибка в команде:\n{e}")
-    except Exception as e:
-        traceback.print_tb(e.__traceback__)
-        bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
-    else:
-        bot.reply_to(message, answer)
-
-@bot.message_handler(content_types=['photo','document'])
-def say_lmao(message: telebot.types.Message):
-        bot.reply_to(message, 'Неверный тип данных!')
+class ConvertException(Exception):
+    pass
 
 
-bot.polling(none_stop=True)
+class Convertor:
+    @staticmethod
+    def get_price(cur1, cur2, amount):
+
+        if cur1 == cur2:
+            raise ConvertException(f'Невозможно перевести одинаковые валюты {base}!')
+
+        if (cur1 == "RUB"):
+            cur1_weght = 1
+        else:
+            etree = ET.fromstring(requests.get('https://www.cbr.ru/scripts/XML_daily.asp').text)
+            val = etree.find('./Valute[CharCode="' + cur1 + '"]/Value')
+            if val == None:
+                raise ConvertException(f"Валюта {cur1} не найдена!")
+            cur1_weght = float(val.text.replace(',', '.'))
+
+        if (cur2 == "RUB"):
+            cur2_weght = 1
+        else:
+            etree = ET.fromstring(requests.get('https://www.cbr.ru/scripts/XML_daily.asp').text)
+            val = etree.find('./Valute[CharCode="' + cur2 + '"]/Value')
+            if val == None:
+                raise ConvertException(f"Валюта {cur2} не найдена!")
+            cur2_weght = float(val.text.replace(',', '.'))
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertException(f'Не удалось обработать количество {amount}!')
+
+        try:
+            result = (cur1_weght/cur2_weght)*amount
+        except ArithmeticError:
+            raise ConvertException(f'Не удалось вычислить !')
+
+        message = f"Цена {amount} {cur1} в {cur2} : {result}"
+        return message
+
+    @staticmethod
+    def get_all_valutes():
+        text = 'RUB\n'
+        for i in ET.fromstring(requests.get('https://www.cbr.ru/scripts/XML_daily.asp').text).findall(
+                './Valute/CharCode'):
+            text += i.text + "\n"
+        return text
+
+    @staticmethod
+    def get_rate(char_code_currency="USD"):
+        if (char_code_currency=="RUB"): return 1
+        return float(ET.fromstring(requests.get('https://www.cbr.ru/scripts/XML_daily.asp').text).find('./Valute[CharCode="'+char_code_currency+'"]/Value').text.replace(',', '.'))
+
+
